3cfef4cfee355f989ec67f15d4471ad71d3cd59e
muhammetemrevatan <emre.vatan98@hotmail.com>
2022-08-19 15:35:08 +0300

    ShortCut

---
+++ src/main/java/com/mev/utils/IntellijShortCut.java
@@ -0,0 +1,11 @@
+package com.mev.utils;
+
+public class IntellijShortCut {
+    /*
+    Setting ==> ctrl+alt+s
+    Alt+Insert ==> constructer / toString / getter,setter
+    Ctrl + / ==> single comment
+    Ctrl+shift+/ ==> multiple comment
+    */
+
+}
