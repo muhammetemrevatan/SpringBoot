03a5b23cbb0e96c96752f7ba98e5d5a8048a8e99
muhammetemrevatan <emre.vatan98@hotmail.com>
2022-08-20 20:26:27 +0300

    PostConstruct

---
+++ src/main/java/com/mev/bean/PostConstructs.java
@@ -0,0 +1,29 @@
+package com.mev.bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
+
+
+public class PostConstructs {
+//    @Autowired
+//    private Logger log;
+//
+////    public PostConstructs() {
+////
+////    }
+//
+//    @PostConstruct
+//    public void init(){
+//        log.info("log info summoned");
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        PostConstructs postConstructs = new PostConstructs();
+//
+//        System.out.println(postConstructs.log);
+//    }
+}
